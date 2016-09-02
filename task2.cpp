83e036221668cd5674e1819b09686c7c5c03e462
setlol <dovnar.maxim@gmail.com>
2016-09-02 12:32:48 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+int star(int);
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int number;
+	cout << "введите количество звёзд = ";
+	cin >> number;
+	star(number);
+}
+
+int star(int number)
+{
+	if (number > 0)
+	{
+		cout << "*";
+		return star(number - 1);
+	}
+	cout << endl;
+}
