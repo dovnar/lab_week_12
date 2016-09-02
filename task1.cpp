02e2af8c75a37ff33657c769ea628195af1275b9
setlol <dovnar.maxim@gmail.com>
2016-09-02 11:26:36 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+int exponent(int, int);
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int number;
+	int extent;
+	cout << "введите число = ";
+	cin >> number;
+	cout << "введите степень числа = ";
+	cin >> extent;
+	cout << number << " в степени " << extent << " = " << exponent(number, extent) << endl;
+}
+
+
+
+
+int exponent(int number, int extent)
+{
+	return (extent > 0 ? (number * exponent(number, extent - 1)) : 1);
+}
